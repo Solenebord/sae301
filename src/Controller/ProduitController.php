4ee37b2e3157cfea6cad0ed9c49fd4f6b32d01e1
Solene <solene.bord@etudiant.univ-reims.fr>
2022-12-04 23:19:44 +0100

    Fin

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\Events;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Lieu;
+use App\Entity\Event;
 
 class ProduitController extends AbstractController
 {
